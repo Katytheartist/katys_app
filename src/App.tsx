cd8da585ffeb33f79771ccb1601a7d6653a832aa
Katy Kane <katytheartist@gmail.com>
2024-09-19 11:08:48 -0500

    check it

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -26,6 +26,13 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <div className="flex flex-col items-center space-y-4 mt-4">
+        <Button label="Click Me" variant="primary" size="medium"/>
+        <Button label="Click Me" variant="secondary" size="medium"/>
+        <Button label="Click Me" variant="secondary" size="small"/>
+        <Button label="Click Me" variant="outlined" icon="😸" iconPosition="right"/>
+        <Button label="Click Me" icon="😸" iconPosition="left"/>
+      </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
@@ -33,4 +40,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
